11de4a6bcfac37c8d18aceefc0eb04355fab1f02
Swapnil Sagar <swapnil.swapnil.sagar946@gmail.com>
2024-10-07 05:20:11 +0530

    Update ServiceCard.tsx

--- webapp/components/Cards/ServiceCard.tsx
+++ webapp/components/Cards/ServiceCard.tsx
@@ -22,30 +22,31 @@ export default function ServiceCard() {
 
   return (
     <div>
-      <div>(Latest five active services)</div>
+      {/* <div>(Latest five active services)</div> */}
       {services.length > 0 ? (
         <div className=' flex flex-col items-center'>
           {services.map((item, i) => {
             return (
               <div
                 key={i}
-                className={`container w-9/12 text-center p-3 border-0 rounded-lg bg-whiteish flex flex-row my-4 `}
+                className={`cards container w-9/12 text-center p-3 border rounded-lg bg-whiteish flex flex-row my-4 `}
               >
-                <div className='bg-blue w-cover h-[19vh] border-0 rounded-lg '>
+                <div className='bg-blue w-cover border-0 rounded-lg '>
                   <Image
                     src={item.ImageUri}
-                    blurDataURL='../../assets/eazy_logo.png'
+                    blurDataURL='../../assets/blur.png'
                     placeholder='blur'
                     alt='service image'
                     width={280}
                     height={210}
+                    className='object-contain rounded-lg mr-4'
                   />
                 </div>
                 <div className='text-xl px-2 w-full'>
-                  <h5 className='text-left text-xl'>Name: {item.name}</h5>
-                  <h5 className='text-left text-xl'>Description: {item.description}</h5>
-                  <h5 className='text-left text-xl'>Plan Duration: {item.planDuration} days</h5>
-                  <h5 className='text-left text-xl'>Plan Price: {item.price} Wei</h5>
+                  <h5 className='text-left text-xl font-semibold'>Name: {item.name}</h5>
+                  <h5 className='text-left text-base'>Description: {item.description}</h5>
+                  <h5 className='text-left text-base'>Plan Duration: {item.planDuration} days</h5>
+                  <h5 className='text-left text-base'>Plan Price: {item.price} Wei</h5>
                 </div>
               </div>
             )
