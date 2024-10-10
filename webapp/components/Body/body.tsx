b44249a295eaf40867f82c61d367a940cd7c2d89
Swapnil Sagar <swapnil.swapnil.sagar946@gmail.com>
2024-10-10 20:05:34 +0530

    Added homescreen carousel

--- webapp/components/Body/body.tsx
+++ webapp/components/Body/body.tsx
@@ -1,19 +1,82 @@
 import Link from 'next/link'
 import { TypeAnimation } from 'react-type-animation'
+import AliceCarousel from 'react-alice-carousel'
+import 'react-alice-carousel/lib/alice-carousel.css'
+
+const responsive = {
+  0: { items: 1 },
+  568: { items: 2 },
+  1024: { items: 3 }
+}
 
 export default function Body() {
+  const handleDragStart = (e: any) => e.preventDefault()
+
+  const items = [
+    <img
+      src='/assets/posters/poster1.jpg'
+      onDragStart={handleDragStart}
+      role='presentation'
+      className='w-full object-cover h-[400px]'
+      key='1'
+    />,
+    <img
+      src='/assets/posters/poster2.jpg'
+      onDragStart={handleDragStart}
+      role='presentation'
+      className='w-full object-cover h-[400px]'
+      key='2'
+    />,
+    <img
+      src='/assets/posters/poster3.jpg'
+      onDragStart={handleDragStart}
+      role='presentation'
+      className='w-full object-cover h-[400px]'
+      key='3'
+    />,
+    <img
+      src='/assets/posters/poster4.jpg'
+      onDragStart={handleDragStart}
+      role='presentation'
+      className='w-full object-cover h-[400px]'
+      key='4'
+    />
+  ]
+
   return (
-    <div className='flex h-[75vh] flex-col lg:mx-64  lg:my-5 my-24 lg:px-4 lg:w-3/4 '>
-      <div className=' text-5xl font-bold text-blue text-start lg:w-3/4 pt-5'>
-        Skip the expensive OTT subscriptions! Buy or rent content directly from your favorite local creators and regional producers.
-        <TypeAnimation
-          sequence={['Videos', 1200, 'Music', 1200, 'Regional Movies', 1200, 'Photos', 1200]}
-          wrapper='span'
-          speed={40}
-          style={{ width: '100%', display: 'inline-block' }}
-          repeat={Infinity}
-        />
-        as NFTs.
+    <div className='flex h-[75vh] flex-col lg:mx-12  lg:my-5 my-24 lg:px-4 '>
+      <div className='flex items-center'>
+        <div className=' text-4xl w-[40%] font-bold text-blue text-start '>
+          Skip the expensive OTT subscriptions! Buy or rent content directly from your favorite
+          local creators and regional producers.
+          <TypeAnimation
+            sequence={['Videos', 1200, 'Music', 1200, 'Regional Movies', 1200, 'Photos', 1200]}
+            wrapper='span'
+            speed={40}
+            style={{ width: '100%', display: 'inline-block' }}
+            repeat={Infinity}
+          />
+          as NFTs.
+        </div>
+        <div className='w-3/5 rounded-lg overflow-hidden'>
+          <AliceCarousel
+            mouseTracking={false}
+            items={items}
+            responsive={responsive}
+            controlsStrategy='responsive'
+            autoPlayDirection='ltr'
+            animationEasingFunction='linear'
+            autoPlay
+            autoPlayStrategy='none'
+            autoPlayInterval={0}
+            animationDuration={3000}
+            animationType='slide'
+            infinite
+            touchTracking={false}
+            disableDotsControls
+            disableButtonsControls
+          />
+        </div>
       </div>
       <div className=' py-10 z-10 w-full'>
         <h1>Choose your profile type</h1>
